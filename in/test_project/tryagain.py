ed33a6293475a82f6efa4a3394d5a4c59ba3722e
skeletus-design <nikita.voro24nin@gmail.com>
2023-09-15 10:35:51 +0300

    front

--- in/test_project/tryagain.py
+++ in/test_project/tryagain.py
@@ -41,23 +41,30 @@ class App:
     #Функция открывает новое окно для добавления данных в бд        
     def add(self):
         self.icon = Tk()
-        self.icon.geometry('600x250')
+        self.icon.geometry('200x250')
         self.icon.resizable('False','False')
+        self.icon.title('')
         
         self.ent2 = ttk.Entry(self.icon)
-        self.ent2.pack(anchor=NW, padx=0, pady=5)
+        self.ent2.pack(anchor=NW)
         self.ent2.get()
+        self.lbl = ttk.Label(self.icon, text='col1')
+        self.lbl.place(x=140, y=1)
         
         self.ent3 = ttk.Entry(self.icon)
-        self.ent3.pack(anchor=NW, padx=0, pady=10)
+        self.ent3.pack(anchor=NW, pady=20)
         self.ent3.get()
+        self.lbl2 = ttk.Label(self.icon, text='col2')
+        self.lbl2.place(x=140, y=41)
         
         self.ent4 = ttk.Entry(self.icon)
-        self.ent4.pack(anchor=NW, padx=0, pady=15)
+        self.ent4.pack(anchor=NW, pady=5)
         self.ent4.get()
+        self.lbl3 = ttk.Label(self.icon, text="col3")
+        self.lbl3.place(x=140, y=85)
         
         bt = ttk.Button(self.icon, text='Добавить', command=self.add_this)
-        bt.place(x=0, y=140)
+        bt.place(x=0, y=180)
         
         
         
@@ -90,8 +97,8 @@ class App:
     #Открытие основного окна, она вызывается первой, следущие функции в основном работают по очереди как каскад
     def main(self):
         self.window = Tk()
-        self.window.geometry('600x600')
-        self.window.configure(bg="blue")
+        self.window.geometry('600x400')
+        self.window.configure(bg="#FFFFE0")
         self.window.resizable('False','False')
         self.lb() #лебл
         self.buttons() #кнопки
@@ -116,10 +123,12 @@ class App:
 
     #Функция кнопок добавить и удалить
     def buttons(self):
+        self.style = ttk.Style()        
+        
         self.bt1 = ttk.Button(text="Добавить", command=self.add)
-        self.bt1.place(x=100,y=450)
+        self.bt1.place(x=100,y=350)
         self.bt2 = ttk.Button(text="Удалить", command=self.delete)
-        self.bt2.place(x=180, y=450)
+        self.bt2.place(x=180, y=350)
         
     def login(self):
         self.login_window = Tk()

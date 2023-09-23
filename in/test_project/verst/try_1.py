f569329ef8f26a816c59d1f91da6edac217dc480
skeletus-design <nikita.voro24nin@gmail.com>
2023-09-23 11:07:29 +0300

    try_3_SQL_Connect

---
+++ in/test_project/verst/try_1.py
@@ -0,0 +1,22 @@
+import pymysql
+
+class App:
+    Con = None
+    
+    def SQL_Connect(self):
+        try:
+            self.Con = pymysql.connect(
+                host="localhost",
+                port=3306,
+                user="root",
+                password="4444",
+                database="team",
+            )
+            print('Connect')
+        except:
+            print('Allert')  
+            
+if __name__ == "__main__":
+    app = App()
+    
+    app.SQL_Connect()

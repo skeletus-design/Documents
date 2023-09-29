8308cc94383aa4cd953caaf5531c02a15417defd
skeletus-design <nikita.voro24nin@gmail.com>
2023-09-29 18:24:14 +0300

    bib

--- in/test_project/verst/app.py
+++ in/test_project/verst/app.py
@@ -2,19 +2,14 @@ import pymysql
 from flask import Flask, render_template, url_for
 
 
-bd = pymysql.connect("localhost", "root", "4444", "team")
-
 app = Flask(__name__)
-api = Api(app)
 
 
 @app.route('/')
 @app.route('/login')
 def index():
     return render_template("login.html")
-def Name():
-    cursor = bd.cursor()
-    sql = "SELECT * FROM "
+
 
 
 @app.route('/reg')
@@ -24,5 +19,4 @@ def reg():
 if __name__ == "__main__":
     app.run(debug=False)
     
-
 

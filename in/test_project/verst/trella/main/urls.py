eb4ec707675efadd48c3c2c0ef182de8c05232c8
skeletus-design <nikita.voro24nin@gmail.com>
2023-09-29 21:50:26 +0300

    ya_vahui

--- in/test_project/verst/trella/main/urls.py
+++ in/test_project/verst/trella/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("sign_in", views.login_, name="login"),
     path("sign_up", views.register, name="register"),
+    path("", views.index, name="Trella"),
 ]
--- in/test_project/verst/trella/main/views.py
+++ in/test_project/verst/trella/main/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html') 
+    else:
+        return redirect('/sign_in')
+
+def login_(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        
     return render(request, 'authorize/login.html')
 
 def register(request):
     return render(request, 'authorize/registration.html')
+
